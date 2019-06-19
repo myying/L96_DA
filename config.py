385e095ef1c8da57feb67a129c852df1f3afd19a
Yue Ying <hardyying@gmail.com>
2019-06-18 22:15:54 -0600

    add some more comments

--- config.py
+++ config.py
@@ -1,22 +1,33 @@
 import numpy as np
-import data_assimilation as DA
+
+###random seed fixed so that results are repeatable
+np.random.seed(0)
+
+### Model setup
 nx = 40
 F = 8.0
 dt = 0.2
-nt = 200
+
+### Cycling experiment setup
+nt = 100
 cycle_period = 1
 time_window = 1 ##smoother analysis window (+-cycles)
+time_space_ratio = 1.0 ##ratio of dt/dx
+
+### Observation network setup
 obs_err = 1
-L = 5  #spatial corr in R
+L = 0  #spatial corr in R
 Lt = 0 #temporal corr in R
-corr_kind = 1  #1:AR(1) 2:AR(2)
-obs_thin = 2
-obs_ind = np.arange(0, nx, obs_thin)
-H = DA.H_matrix(nx, time_window, obs_ind)
-R = DA.R_matrix(nx, time_window, obs_ind, obs_err, L, Lt, corr_kind)
+##uniform network
+obs_thin = 0.2
+obs_ind = np.tile(np.arange(0, nx, obs_thin), (nt, 1)).T
+##random network
+# nobs = 30
+# obs_ind = np.random.uniform(0, nx, size=(nobs, nt))
+
+### Ensemble filter tuning parameters
 nens = 20
 ROI = 10  #localization in space (grid points)
-ROIt = 0  #localization in time (cycles)
-rho = DA.local_matrix(nx, time_window, ROI, ROIt)
-alpha = 0.0
-inflation = 1.0
+ROIt = 2  #localization in time (cycles)
+alpha = 0.0  ##relaxation coef
+inflation = 1.0  ##multiplicative inflation
