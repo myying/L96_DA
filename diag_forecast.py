57dbee3d54b927e1ddefdfc043bbfd21307b1e8f
Yue Ying <hardyying@gmail.com>
2019-06-25 10:39:04 -0600

    diagnose forecast error spectrum

---
+++ diag_forecast.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import numpy as np
+import misc
+import data_assimilation as DA
+import config as p
+import matplotlib.pyplot as plt
+import sys
+from scipy.fftpack import fft
+plt.switch_backend('Agg')
+plt.figure(figsize=(8, 6))
+
+outdir = sys.argv[1]
+tt = int(sys.argv[2])
+truth = np.load(outdir+"/truth.npy")
+prior = np.load(outdir+"/ensemble_forecast.npy")
+nx, nens, nt = prior.shape
+
+###covariance matrices
+Pb = np.zeros((nx, nx))
+for t in range(nt):
+  Pb += misc.error_covariance(prior[:, :, t]) 
+Pb = Pb/nt
+# Pb = misc.error_covariance(prior[:, :, tt])
+
+##actual error matrices
+Qb = misc.Q_out(prior, truth)
+# Qb = misc.Q_out(prior[:, :, tt:tt+1], truth[:, tt:tt+1])
+print('prior rmse = {}, sprd = {}'.format(misc.rmse(Qb), misc.sprd(Pb)))
+
+##spectrum
+Lb = misc.matrix_spec(Pb)
+Lbt = misc.matrix_spec(Qb)
+
+###plot eigenvalue spectrum
+ax = plt.subplot(221)
+ax.plot(Lbt, 'b', label=r'$\Lambda^{b*}$')
+ax.plot(Lb, 'c', label=r'$\Lambda^b$')
+# ax.legend(fontsize=13, ncol=2)
+ax.set_ylim(0, 5)
+ax.set_xlim(-1, p.nx/2)
+
+plt.savefig('1.pdf')
--- run_forecast.py
+++ run_forecast.py
@@ -10,10 +10,10 @@ xt = np.load("output/truth.npy")
 yo = np.load("output/obs.npy")
 xens0 = np.load("output/initial_ensemble.npy")
 
-xens = np.zeros([p.nx, p.nens, p.nt+1])
+xens = np.zeros([p.nx, p.nens, p.nt])
 xens[:, :, 0] = xens0[:, 0:p.nens]
 
-for tt in np.arange(p.nt):
+for tt in np.arange(p.nt-1):
   xens[:, :, tt+1] = L96.M_nl(xens[:, :, tt], p.nx, p.F, p.dt)
 
 np.save("output/ensemble_forecast", xens)
